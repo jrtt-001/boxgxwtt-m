76e7f59b0c48770e774ec35b4db60251fec6a080
jrtt <jrttitcase.cn>
2021-02-26 18:37:40 +0800

    项目初始化-引入Vant组件库

--- boxgxwtt-m/src/App.vue
+++ boxgxwtt-m/src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="app">
     <router-view />
+
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+
+    <van-cell title="单元格" value="内容" size="large" />
+    <van-cell title="单元格" value="内容" size="large" label="描述信息" />
   </div>
 </template>
 
